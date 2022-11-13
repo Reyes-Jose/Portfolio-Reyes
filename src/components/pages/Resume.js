44c5bc9f567e78690837e5948bfbf32afdd919d5
JoeDoom <reyesj1209@gmail.com>
2022-11-12 22:01:17 -0800

    fixes resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,47 +1,79 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-import { CardGroup } from "react-bootstrap/CardGroup";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import { BsDownload } from "react-icons/bs";
 
 const container = {
-      
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute",
-        height: "80%",
-        width: "100%",
-       
-}
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "absolute",
+  height: "80%",
+  width: "100%",
+};
 
+export default function Resume() {
+  return (
+    <div style={container}>
+      <CardGroup style={{ width: "50rem", gap: "3rem" }}>
+        <Card style={{ background: "#121212" }}>
+          <Card.Body>
+            <Card.Title
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Front End:
+            </Card.Title>
+            <Card.Text>
+              <li>Html</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>Jquery</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+              <li>Bulma</li>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ background: "#121212" }}>
+          <Card.Body>
+            <Card.Title
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Back End:
+            </Card.Title>
+            <Card.Text>
+              <li>Express</li>
+              <li>Node</li>
+              <li>mySQL</li>
+              <li>noSQL(mongoDB, Mongoose)</li>
+              <li>Object Relational Mapping(ORM)</li>
+              <li>Model View Controller(MVC)</li>
+              <li>Progressive Web Applications(PWA)</li>
+              <li>GraphQL</li>
+              <li>API's</li>
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-
-export default function Resume(){
-    return(
-        <div>
-            <div style={container}>
-            <ul>
-                <h2>Front End: </h2>
-                <li>Html</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>Jquery</li>
-                <li>React</li>
-                <li>Bootstrap</li>
-                <li>Bulma</li>
-            </ul>
-            <ul>
-                <h2>Back End:</h2>
-                <li>Express</li>
-                <li>Node</li>
-                <li>mySQL</li>
-                <li>noSQL(mongoDB, Mongoose)</li>
-                <li>Object Relational Mapping(ORM)</li>
-                <li>Model View Controller(MVC)</li>
-                <li>Progressive Web Applications(PWA)</li>
-                <li>GraphQL</li>
-                <li>API's</li>
-            </ul>
-            </div>
-        </div>
-    )
+        <BsDownload
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            height: "90%",
+          }}
+          size={20}
+        ></BsDownload>
+      </CardGroup>
+    </div>
+  );
 }
